d660469f69e65d3a6e5e7b851075bc1fad8d6cb3
Darshil Shah <darshilx@amazon.com>
2022-10-09 18:35:48 +0530

    03 : Create a Projectile

--- index.js
+++ index.js
@@ -21,9 +21,32 @@ class Player {
   }
 }
 
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+}
+
 const playerPosX = canvas.width / 2;
 const playerPosY = canvas.height / 2;
 
 
 const player = new Player(playerPosX, playerPosY, 30, 'blue')
 player.draw()
+
+window.addEventListener('click', (event) => {
+  console.log(event)
+  const projectile = new Projectile(event.clientX, event.clientY, 5, 'red', null)
+  projectile.draw()
+})
