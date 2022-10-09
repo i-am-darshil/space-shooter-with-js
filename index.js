ec06098483033cfceaf57acaf46b7eeb2b797407
Darshil Shah <darshilx@amazon.com>
2022-10-09 19:02:40 +0530

    05 : Creating Enemies Part 1

--- index.js
+++ index.js
@@ -44,6 +44,29 @@ class Projectile {
   }
 }
 
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+
+  draw() {
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+    ctx.fillStyle = this.color
+    ctx.fill()
+  }
+
+  update() {
+    this.draw()
+    this.x += this.velocity.x
+    this.y += this.velocity.y
+  }
+}
+
 const playerPosX = canvas.width / 2;
 const playerPosY = canvas.height / 2;
 
@@ -51,6 +74,28 @@ const playerPosY = canvas.height / 2;
 const player = new Player(playerPosX, playerPosY, 30, 'blue')
 
 const projectiles = []
+const enemies = []
+
+
+function spawnEnemies() {
+  setInterval(() => {
+    const x = 100
+    const y = 100
+    const radius = 30
+    const color = 'green'
+    const angle = Math.atan2(
+      playerPosY - y,
+      playerPosX - x
+    )
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle)
+    }
+    enemies.push(
+      new Enemy(x, y, radius, color, velocity)
+    )
+  }, 1500)
+}
 
 function animate() {
   window.requestAnimationFrame(animate)
@@ -58,6 +103,10 @@ function animate() {
   player.draw()
   projectiles.forEach(projectile => {
     projectile.update()
+  })
+
+  enemies.forEach((enemy) => {
+    enemy.update()
   })
 }
 
@@ -83,3 +132,4 @@ window.addEventListener('click', (event) => {
 })
 
 animate()
+spawnEnemies()
