3c093a27520ba1501f72e57781a7b7d3742ff3b5
Darshil Shah <darshilx@amazon.com>
2022-10-09 20:13:35 +0530

    08 : Remove projectiles when off-the screen

--- index.js
+++ index.js
@@ -110,8 +110,19 @@ function animate() {
   animationId = window.requestAnimationFrame(animate)
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   player.draw()
-  projectiles.forEach(projectile => {
+  projectiles.forEach((projectile, projectileIndex) => {
     projectile.update()
+
+    if (
+      projectile.x - projectile.radius < 0 || 
+      projectile.x - projectile.radius > canvas.width || 
+      projectile.y - projectile.radius < 0 || 
+      projectile.y - projectile.radius > canvas.height
+      ) {
+        projectiles.splice(projectileIndex, 1)
+        console.log("Removing projectile")
+    }
+
   })
 
   enemies.forEach((enemy, enemyIndex) => {
